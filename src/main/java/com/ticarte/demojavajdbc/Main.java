c849eaf18b9cd42244284ded05f8ab8be3788219
RafaTicArte <rafa@ticarte.com>
2024-03-18 10:43:35 +0100

    Returns and prepared statements

--- src/main/java/com/ticarte/demojavajdbc/Main.java
+++ src/main/java/com/ticarte/demojavajdbc/Main.java
@@ -1,13 +1,12 @@
 package com.ticarte.demojavajdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int rows = 0;
+
+        System.out.println("Hello Java JDBC!");
 
         try {
             Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/java", "root", "");
@@ -20,27 +19,39 @@ public class Main {
                     "year INT, " +
                     "director LONGTEXT)");
 
-            stmt.executeUpdate("INSERT INTO films (name, year, director) " +
+            rows = stmt.executeUpdate("INSERT INTO films (name, year, director) " +
                     "VALUES ('The Shawshank Redemption', 1994, 'Frank Darabont')");
-            stmt.executeUpdate("INSERT INTO films (name, year, director) " +
+            System.out.println("Number of rows inserted: " + rows);
+            rows = stmt.executeUpdate("INSERT INTO films (name, year, director) " +
                     "VALUES ('The Godfather', 1972, 'Francis Ford Coppola')");
-            stmt.executeUpdate("INSERT INTO films (name, year, director) " +
+            System.out.println("Number of rows inserted: " + rows);
+            rows = stmt.executeUpdate("INSERT INTO films (name, year, director) " +
                     "VALUES ('The Dark Knight', 2008, 'Christopher Nolan')");
-            stmt.executeUpdate("INSERT INTO films (name, year, director) " +
+            System.out.println("Number of rows inserted: " + rows);
+            rows = stmt.executeUpdate("INSERT INTO films (name, year, director) " +
                     "VALUES ('The Lord of the Rings: The Return of the King', 2003, 'Peter Jackson')");
-            stmt.executeUpdate("INSERT INTO films (name, year, director) " +
+            System.out.println("Number of rows inserted: " + rows);
+            rows = stmt.executeUpdate("INSERT INTO films (name, year, director) " +
                     "VALUES ('Pulp Fiction', 1994, 'Quentin Tarantino')");
-            stmt.executeUpdate("INSERT INTO films (name, year, director) " +
+            System.out.println("Number of rows inserted: " + rows);
+            rows = stmt.executeUpdate("INSERT INTO films (name, year, director) " +
                     "VALUES ('Forrest Gump', 1994, 'Robert Zemeckis')");
+            System.out.println("Number of rows inserted: " + rows);
 
-            stmt.executeUpdate("UPDATE films SET director = 'Frank Darabont (*)' " +
+            rows  = stmt.executeUpdate("UPDATE films SET director = 'Frank Darabont (*)' " +
                     "WHERE name = 'The Shawshank Redemption'");
+            System.out.println("Number of rows updated: " + rows);
 
-            stmt.executeUpdate("DELETE FROM films " +
+            rows = stmt.executeUpdate("DELETE FROM films " +
                     "WHERE name = 'The Godfather'");
+            System.out.println("Number of rows deleted: " + rows);
 
             System.out.println("List of films:");
             ResultSet rs = stmt.executeQuery("SELECT * FROM films");
+            if (rs.last()) {
+                System.out.println("Number of films: " + rs.getRow());
+                rs.beforeFirst();
+            }
             while (rs.next()) {
                 System.out.println(rs.getInt("id") + ". " +
                         rs.getString("name") + " (" +
@@ -48,8 +59,21 @@ public class Main {
                         rs.getString("director") + "]");
             }
 
-            System.out.println("List of films from 1994:");
+            System.out.println("List of films from 1994 with raw statement:");
             rs = stmt.executeQuery("SELECT * FROM films WHERE year = 1994");
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + ". " +
+                        rs.getString("name") + " (" +
+                        rs.getInt("year") + ") [" +
+                        rs.getString("director") + "]");
+            }
+
+            System.out.println("List of films from 1994 with prepared statement:");
+            String stmtSelectFilms = "SELECT * FROM films WHERE year >= ?";
+            PreparedStatement prepareStmt = connection.prepareStatement(stmtSelectFilms);
+            prepareStmt.setInt(1, 1994);
+            // Depurando se puede visualizar la consulta preparada con sus valores.
+            rs = prepareStmt.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getInt("id") + ". " +
                         rs.getString("name") + " (" +
